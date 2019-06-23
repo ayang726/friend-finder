247002dd8f07ee00749f5cf034d22f4d33a3cb73
AwesomePresent <yoursmile128@hotmail.com>
2019-06-22 20:21:26 -0400

    Added view data functionality

--- app/routing/apiRoutes.js
+++ app/routing/apiRoutes.js
@@ -2,7 +2,7 @@ const friends = require("../data/friends");
 
 module.exports = function (app) {
     app.get("/api/friends", (req, res) => {
-        res.json(friends);
+        res.send(friends);
     });
 
     app.post("/api/friends", (req, res) => {
